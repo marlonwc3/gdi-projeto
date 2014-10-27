0cddbfc3396ed14fa69695fec52a48b52b03ec10
Tomer Simis <tdl.simis@gmail.com>
2014-10-27 20:43:28 -0300

    Create loja.sql

---
+++ loja.sql
@@ -0,0 +1,30 @@
+# Loja
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('86.814.375/0001-00', 'Roupas', 'contato@cea.com.br', 'Jovem', 'Á vista/Á prazo', SYSDATE, 'CeA');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('83.824.371/0001-00', 'Restaurante', 'contato@boteco.com.br', 'Adulto', 'Á vista/Á prazo', SYSDATE, 'Boteco - Bar e Restaurante');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('33.824.141/0002-00', 'Farmácia', 'contato@santamaria.com.br', 'Idoso', 'Á vista/Á prazo', SYSDATE, 'Farmácia Santa Maria');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('13.14.141/132-00', 'Restaurante', 'hablaconosco@donandaluz.com.br', 'Maratonista', 'Dinero', SYSDATE, 'Don Andaluz');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('13.11.111/112-00', 'Livraria', 'contato@saraiva.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Livraria Saraiva');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('23.13.131/112-12', 'Ótica', 'contato@oticasdiniz.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Óticas Diniz');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('23.11.121/132-11', 'Cinema', 'contato@uci.com.br', 'Jovem', 'Á vista/Á prazo', SYSDATE, 'Cinema UCI');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('93.15.128/112-11', 'Loja', 'contato@eletroshopping.com.br', 'Adulto', 'Á vista/Á prazo', SYSDATE, 'Eletroshopping');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('82.43.132/193-63', 'Mercado', 'contato@hiper.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Hiper-Bombreço');
+
+INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
+VALUES ('58.34.321/132-43', 'Loja', 'contato@rommanel.com.br', 'Adulto', 'Á vista', SYSDATE, 'Rommanel Jóias');
