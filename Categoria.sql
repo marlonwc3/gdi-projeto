d768c61a0f775d91195782f6945e647105db32ed
mras <mras@cin.ufpe.br>
2014-10-27 19:53:58 -0300

    Povoamento de Categoria

---
+++ Categoria.sql
@@ -0,0 +1,31 @@
+#Categoria 
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Flashdog', 'in ante vestibulum ante ipsum primis in faucibus orci');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Brightdog', 'natoque penatibus et magnis dis parturient montes nascetur');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Aivee', 'duis at velit eu est congue elementum');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Mydo', 'urna pretium nisl ut volutpat sapien');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Kazio', 'hac habitasse platea dictumst morbi vestibulum');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Yakidoo', 'in hac habitasse platea dictumst etiam faucibus cursus urna ut');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Wordpedia', 'odio justo sollicitudin ut suscipit a feugiat');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Trupe', 'quisque porta volutpat erat quisque erat');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Linkbridge', 'metus vitae ipsum aliquam non mauris morbi non');
+
+INSERT INTO Categoria (categoria, Categoria_Descricao) 
+VALUES ('Zoovu', 'felis donec semper sapien a libero');
