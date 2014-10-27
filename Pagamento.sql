c3c27888a5c8eba6dd04113f944e569e6d15e9c7
unknown <lre@cin.ufpe.br>
2014-10-27 20:53:56 -0300

    aluguel e pagamento

---
+++ Pagamento.sql
@@ -0,0 +1,29 @@
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (10022334, SYSDATE , 'A vista', 1000.20);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (10034, SYSDATE, 'Boleto', 2953.70);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (1004, SYSDATE, 'Credito', 765.80);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (10, SYSDATE, 'Credito', 674.00);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (1002, SYSDATE, 'A vista', 235.50);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (4, SYSDATE, 'Boleto', 321.80);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (14, SYSDATE, 'Deposito', 500.90);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (124, SYSDATE, 'Credito', 150.10);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (10334, SYSDATE, 'A vista', 750.20);
+
+insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
+values (9422334, SYSDATE, 'Deposito', 343.20);
