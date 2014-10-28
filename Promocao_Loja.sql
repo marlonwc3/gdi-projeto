95bb501f70826529ec03152ef55c708d1292e98c
Tomer Simis <tdl.simis@gmail.com>
2014-10-28 20:57:44 -0300

    Create Promocao_Loja.sql

---
+++ Promocao_Loja.sql
@@ -0,0 +1,29 @@
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('86.814.375/0001-00', 26540, 'Calçados', 12.40, 'in ante vestibulum ante ipsum primis in faucibus orci', SYSDATE);
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('83.824.371/0001-00', 21807, 'Vestidos', 29.57, 'natoque penatibus et magnis dis parturient montes nascetur', TO_DATE('09/07/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('33.824.141/0002-00', 21971, 'Eletrônicos', 33.65, 'duis at velit eu est congue elementum', TO_DATE('12/06/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('13.14.141/132-00', 89937, 'Óculos de grau', 21.48, 'urna pretium nisl ut volutpat sapien', SYSDATE);
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('13.11.111/112-00', 1234, 'Televisões', 47.72, 'hac habitasse platea dictumst morbi vestibulum', TO_DATE('01/04/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('23.13.131/112-12', 89316, 'Consolas', 62.39, 'in hac habitasse platea dictumst etiam faucibus cursus urna ut', SYSDATE);
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('23.11.121/132-11', 75305, 'Remédios', 15.68, 'odio justo sollicitudin ut suscipit a feugiat', TO_DATE('16/09/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('93.15.128/112-11', 39719, 'Celulares', 61.33, 'quisque porta volutpat erat quisque erat', TO_DATE('29/08/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('82.43.132/193-63', 89292, 'Cosméticos', 47.11, 'metus vitae ipsum aliquam non mauris morbi non', TO_DATE('13/09/2014', 'dd/mm/yyyy'));
+
+INSERT INTO Promocao_Loja (Loja_CNPJ, Promocao_Codigo, categoria, Porcentagem, Descricao_Promocao, Data_inicio)
+VALUES ('58.34.321/132-43', 43766, 'Esportivos', 29.57, 'felis donec semper sapien a libero', TO_DATE('05/09/2014', 'dd/mm/yyyy'));
