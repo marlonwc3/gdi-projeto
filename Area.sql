7b46fb8cf96e8175e4655cd045180564eb45241f
unknown <mras@G4C30.windows.cin.ufpe.br>
2014-10-28 09:42:42 -0300

    Area

---
+++ Area.sql
@@ -0,0 +1,30 @@
+#Area
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (1, 'Externa', 'Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.', 74.18, 20.9, 40.61);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (2, 'Mista', 'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna.', 25.83, 26.86, 19.64);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (3, 'Mista', 'Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh.', 70.47, 4.43, 45.65);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (4, 'Externa', 'Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.', 81.34, 86.67, 97.09);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (5, 'Interna', 'In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 60.69, 32.25, 87.99);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (6, 'Mista', 'Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio.', 77.11, 64.44, 60.45);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (7, 'Externa', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 68.75, 51.31, 38.62);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (8, 'Interna', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 93.67, 2.44, 6.19);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (9, 'Interna', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 67.42, 37.3, 85.53);
+
+INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
+VALUES (10, 'Mista', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 93.18, 91.7, 8.43);
