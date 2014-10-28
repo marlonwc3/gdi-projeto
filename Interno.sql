71f051355a8bef454894f3c07835677cf97d641b
unknown <lre@cin.ufpe.br>
2014-10-27 21:44:49 -0300

    atualizando

---
+++ Interno.sql
@@ -0,0 +1,29 @@
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-1', '000.000.000-10', SYSDATE, 'Gerente');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-2', '000.000.000-10', SYSDATE, 'Atendente');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-3', '000.000.000-5', SYSDATE, 'Segurança');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-4', '000.000.000-5', SYSDATE, 'Segurança');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-5', '000.000.000-1', SYSDATE, 'Gerente');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-6', '000.000.000-1', SYSDATE, 'Segurança');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-7', '000.000.000-1', SYSDATE, 'Atendente');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-8', '000.000.000-5', SYSDATE, 'Atendente');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-9', '000.000.000-10', SYSDATE, 'Higienizador');
+
+insert into Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
+	values ('000.000.000-10', '000.000.000-5', SYSDATE, 'Gerente');
---
+++ Telefone.sql
@@ -0,0 +1,29 @@
+insert into Telefone (CNPJ, Numero) 
+	values ('86.814.375/0001-00', '3943-4022');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('83.824.371/0001-00', '3243-2022');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('33.824.141/0002-00', '3193-1022');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('13.14.141/132-00', '3503-5022');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('13.11.111/112-00', '3483-6029');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('23.13.131/112-12', '3373-1022');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('23.11.121/132-11', '3353-0027');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('93.15.128/112-11', '3143-5025');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('82.43.132/193-63', '3243-4032');
+
+insert into Telefone (CNPJ, Numero) 
+	values ('58.34.321/132-43', '3343-5021');
