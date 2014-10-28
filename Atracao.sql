d0a42db7fa57c5270f1ea100beee91c34f020fd8
Tomer Simis <tdl.simis@gmail.com>
2014-10-27 22:12:53 -0300

    Create Atracao.sql

---
+++ Atracao.sql
@@ -0,0 +1,20 @@
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (4, 'Banda Polentinha do Arrocha', 'Axé', 'Música', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur blandit arcu eget laoreet. Etiam auctor massa ac urna condimentum semper. ');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (1, 'Palhaço Chocolate', 'Brincadeiras', 'Humor', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (2, 'Victor e Leo', 'É festa de rodeio', 'Música', 'Vestibulum ut leo nec ex efficitur posuere. Vivamus sapien felis, ornare eu enim et, commodo maximus orci.');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (3, 'Marlon Marley', 'Como tocar cuíca', 'Workshop', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non lacus nibh. Morbi euismod sodales ligula auctor pretium.');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (6, 'DJ Samuel', 'Party EletroPetro', 'Música', 'Phasellus lacinia nibh et elit sollicitudin vehicula. Proin et nunc lobortis mauris tempus tincidunt. Sed dictum congue nunc, ac lobortis lacus sodales id.');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (1, 'Danilo Gentily', 'Eleições', 'Stand-Up', 'Fusce urna nibh, ultricies semper tellus eu, dignissim volutpat odio. Aenean aliquet, felis interdum semper consectetur, turpis tellus feugiat erat.');
+
+INSERT INTO Atracao (Codigo_evento, Nome, Tema, Tipo, Descricao)
+VALUES (1, 'Shevchenko e Elloco', 'Brega de cada dia', 'Música', 'Ultricies semper tellus eu, dignissim volutpat odio.');
