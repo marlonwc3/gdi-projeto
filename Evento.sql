ec743dbf09f187f2058f483d922071a95b6eb0fd
SamuelPazMendes <spm@cin.ufpe.br>
2014-10-28 19:47:27 -0200

    Create Evento.sql

---
+++ Evento.sql
@@ -0,0 +1,6 @@
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(1, 'Festa', SYSDATE,SYSDATE,'Publico');
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(2, 'Rodizio', SYSDATE,SYSDATE,'Privado');
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(3, 'Festanca', SYSDATE,SYSDATE,'Privado') ;
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(4, 'Reggae', SYSDATE,SYSDATE,'Public') ;
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(5, 'Show', SYSDATE,SYSDATE,'Publico') ;
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(6, 'Festa', SYSDATE,SYSDATE,'Privado') ;
