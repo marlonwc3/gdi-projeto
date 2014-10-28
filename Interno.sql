9405988a322b72338f4876e245fb158b1eac09ce
SamuelPazMendes <spm@cin.ufpe.br>
2014-10-28 21:45:01 -0200

    Update Interno.sql

--- Interno.sql
+++ Interno.sql
@@ -1,30 +1,30 @@
 # Interno
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-1', '000.000.000-10', SYSDATE, 'Gerente');
+	VALUES ('000.000.000-1', '000.000.000-0', SYSDATE, 'Gerente');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-2', '000.000.000-10', SYSDATE, 'Atendente');
+	VALUES ('000.000.000-2', '000.000.000-0', SYSDATE, 'Atendente');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-3', '000.000.000-5', SYSDATE, 'Segurança');
+	VALUES ('000.000.000-3', '000.000.000-0', SYSDATE, 'Segurança');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-4', '000.000.000-5', SYSDATE, 'Segurança');
+	VALUES ('000.000.000-4', '000.000.000-0', SYSDATE, 'Segurança');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-5', '000.000.000-1', SYSDATE, 'Gerente');
+	VALUES ('000.000.000-5', '000.000.000-2', SYSDATE, 'Gerente');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-6', '000.000.000-1', SYSDATE, 'Segurança');
+	VALUES ('000.000.000-6', '000.000.000-3', SYSDATE, 'Segurança');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-7', '000.000.000-1', SYSDATE, 'Atendente');
+	VALUES ('000.000.000-7', '000.000.000-4', SYSDATE, 'Atendente');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
 	VALUES ('000.000.000-8', '000.000.000-5', SYSDATE, 'Atendente');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-9', '000.000.000-10', SYSDATE, 'Higienizador');
+	VALUES ('000.000.000-9', '000.000.000-6', SYSDATE, 'Higienizador');
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao) 
-	VALUES ('000.000.000-10', '000.000.000-5', SYSDATE, 'Gerente');
+	VALUES ('000.000.000-10', '000.000.000-7', SYSDATE, 'Gerente');
