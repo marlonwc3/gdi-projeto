e52cf7b9b316357387a1c812542118f9d3ac1fe8
Tomer Simis <tdl.simis@gmail.com>
2014-10-28 13:57:30 -0300

    Update Aluguel.sql

--- Aluguel.sql
+++ Aluguel.sql
@@ -1,29 +1,29 @@
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (1, SYSDATE, 1194.65, 'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (1, SYSDATE, 1194.65, 'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao)
-values (2, SYSDATE , 608.6800000001, 'fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao)
+VALUES (2, SYSDATE , 608.6800000001, 'fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (3, SYSDATE, 1812.11, 'nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (3, SYSDATE, 1812.11, 'nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (4, SYSDATE, 2444.23, 'mi nulla ac enim in tempor turpis nec euismod scelerisque');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (4, SYSDATE, 2444.23, 'mi nulla ac enim in tempor turpis nec euismod scelerisque');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (5, SYSDATE, 977687.91, 'pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (5, SYSDATE, 977687.91, 'pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao)
-values (6, SYSDATE, 538.33, 'amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao)
+VALUES (6, SYSDATE, 538.33, 'amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (7, SYSDATE, 962.74, 'quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (7, SYSDATE, 962.74, 'quis justo maecenas rhoncus aliquam lacus morbi quis tortor id nulla ultrices aliquet');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (8, SYSDATE, 9355.11, 'pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut erat id mauris vulputate');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (8, SYSDATE, 9355.11, 'pretium iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut erat id mauris vulputate');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (9, SYSDATE, 1512.81, 'posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (9, SYSDATE, 1512.81, 'posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum primis in');
 
-insert into Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
-values (10, SYSDATE, 1277.01, 'magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque');
+INSERT INTO Aluguel (Codigo, Data_de_vencimento, Valor, Descricao) 
+VALUES (10, SYSDATE, 1277.01, 'magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque');
