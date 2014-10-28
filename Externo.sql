07c898894be85bcba5faf36e4d1902112de0b108
SamuelPazMendes <spm@cin.ufpe.br>
2014-10-28 20:01:53 -0200

    Create Externo.sql

---
+++ Externo.sql
@@ -0,0 +1,5 @@
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '1','Baixo','Total');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '2','Alto','Total');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '3','Alto','ParcialB1');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '4','Baixo','ParcialB2');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '5','Baixo','ParcialB3');
