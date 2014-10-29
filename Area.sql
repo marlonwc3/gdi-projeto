3cf88d0459b17fb2cc8baaf9564e01dfdc10359f
SamuelPazMendes <spm@cin.ufpe.br>
2014-10-28 22:11:23 -0200

    Update Area.sql

--- Area.sql
+++ Area.sql
@@ -1,30 +1,30 @@
 # Area
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (1, 'Externa', 'Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo..', 74.18, 20.9, 40.61);
+VALUES (1, 'Externa', ' Etiam justo. Etiam pretium iaculis justo..', 74.18, 20.9, 40.61);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (2, 'Mista', 'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue..', 25.83, 26.86, 19.64);
+VALUES (2, 'Mista', ' ut volutpat sapien arcu sed augue..', 25.83, 26.86, 19.64);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (3, 'Mista', 'Proin at turpis a pede posuere nonummy. Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.', 70.47, 4.43, 45.65);
+VALUES (3, 'Mista', 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.', 70.47, 4.43, 45.65);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (4, 'Externa', 'Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.', 81.34, 86.67, 97.09);
+VALUES (4, 'Externa', ' Etiam faucibus cursus urna. Ut tellus.  Nullam varius.', 81.34, 86.67, 97.09);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (5, 'Interna', 'In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', 60.69, 32.25, 87.99);
+VALUES (5, 'Interna', '  turpis enim blandit mi, in porttitor pede justo eu massa.', 60.69, 32.25, 87.99);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (6, 'Mista', 'Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio.', 77.11, 64.44, 60.45);
+VALUES (6, 'Mista', '  Integer ac leo. Pellentesque ultrices mattis odio.', 77.11, 64.44, 60.45);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (7, 'Externa', 'Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 68.75, 51.31, 38.62);
+VALUES (7, 'Externa', ' nibh ligula nec sem.', 68.75, 51.31, 38.62);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (8, 'Interna', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', 93.67, 2.44, 6.19);
+VALUES (8, 'Interna', 'erra dapibus. Nulla suscipit ligula in lacus.', 93.67, 2.44, 6.19);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
-VALUES (9, 'Interna', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem.', 67.42, 37.3, 85.53);
+VALUES (9, 'Interna', 'llis molestie lorem.', 67.42, 37.3, 85.53);
 
 INSERT INTO Area (Codigo, Tipo, Descricao , Altura , Comprimento , Largura ) 
 VALUES (10, 'Mista', 'Praesent blandit. Nam nulla.', 93.18, 91.7, 8.43);
