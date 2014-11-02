719a45e930b3caace64f67bf8556b6e29316c51c
Tomer Simis <tdl.simis@gmail.com>
2014-11-01 23:30:21 -0300

    Update Evento.sql

--- Evento.sql
+++ Evento.sql
@@ -1,6 +1,17 @@
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(1, 'Festa', SYSDATE,SYSDATE,'Publico');
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(2, 'Rodizio', SYSDATE,SYSDATE,'Privado');
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(3, 'Festanca', SYSDATE,SYSDATE,'Privado') ;
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(4, 'Reggae', SYSDATE,SYSDATE,'Public') ;
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(5, 'Show', SYSDATE,SYSDATE,'Publico') ;
-INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade) VALUES(6, 'Festa', SYSDATE,SYSDATE,'Privado') ;
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(1, 'Festa', TO_DATE('05/11/2014', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Publico');
+
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(2, 'Rodizio', TO_DATE('17/11/2014', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Privado');
+
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(3, 'Festanca', TO_DATE('23/12/2014', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Privado');
+
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(4, 'Reggae', TO_DATE('24/12/2014', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Public');
+
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(5, 'Show', TO_DATE('14/11/2014', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Publico');
+
+INSERT INTO Evento (Codigo,Descricao,Data_de_inicio,Data_de_fim,Privacidade)
+VALUES(6, 'Festa', TO_DATE('05/01/2015', 'dd/mm/yyyy'),TO_DATE('05/11/2014', 'dd/mm/yyyy'),'Privado');
