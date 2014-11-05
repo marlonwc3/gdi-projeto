2cf4424b2795dab97cb1d64c66dc02fcb2c981be
pazspm <pazspm@users.noreply.github.com>
2014-11-05 18:45:54 -0300

    Update Interno.sql

--- Interno.sql
+++ Interno.sql
@@ -1,30 +1,30 @@
 ﻿# Interno
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario) 
-	VALUES ('000.000.000-1', '000.000.000-0', SYSDATE, 'Gerente', 3000);
+	VALUES ('000.000.000-1', '000.000.000-0', TO_DATE('12/07/2014', 'dd/mm/yyyy'), 'Gerente', 3000);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-2', '000.000.000-0', SYSDATE, 'Atendente', 2000);
+	VALUES ('000.000.000-2', '000.000.000-0',TO_DATE('11/07/2014', 'dd/mm/yyyy'), 'Atendente', 2000);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario) 
-	VALUES ('000.000.000-3', '000.000.000-0', SYSDATE, 'Segurança', 1000);
+	VALUES ('000.000.000-3', '000.000.000-0', TO_DATE('12/01/2011', 'dd/mm/yyyy'), 'Segurança', 1000);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-4', '000.000.000-0', SYSDATE, 'Segurança', 2500);
+	VALUES ('000.000.000-4', '000.000.000-0', TO_DATE('03/07/2010', 'dd/mm/yyyy'), 'Segurança', 2500);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-5', '000.000.000-2', SYSDATE, 'Gerente', 500);
+	VALUES ('000.000.000-5', '000.000.000-2', TO_DATE('17/07/2004', 'dd/mm/yyyy'), 'Gerente', 500);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-6', '000.000.000-3', SYSDATE, 'Segurança', 1300);
+	VALUES ('000.000.000-6', '000.000.000-3', TO_DATE('12/07/2004', 'dd/mm/yyyy'), 'Segurança', 1300);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-7', '000.000.000-4', SYSDATE, 'Atendente', 1000);
+	VALUES ('000.000.000-7', '000.000.000-4', TO_DATE('02/07/2004', 'dd/mm/yyyy'), 'Atendente', 1000);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-8', '000.000.000-5', SYSDATE, 'Atendente', 2400);
+	VALUES ('000.000.000-8', '000.000.000-5', TO_DATE('02/07/2003', 'dd/mm/yyyy'), 'Atendente', 2400);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-9', '000.000.000-6', SYSDATE, 'Higienizador', 2500);
+	VALUES ('000.000.000-9', '000.000.000-6', TO_DATE('03/07/2007', 'dd/mm/yyyy'), 'Higienizador', 2500);
 
 INSERT INTO Interno (CPF, CPF_Supervisor, Data_de_contratacao, Funcao, Salario)  
-	VALUES ('000.000.000-10', '000.000.000-7', SYSDATE, 'Gerente', 4600);
+	VALUES ('000.000.000-10', '000.000.000-7', TO_DATE('12/07/2004', 'dd/mm/yyyy'), 'Gerente', 4600);
