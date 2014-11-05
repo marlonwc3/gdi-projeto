f110980abe384d1ab2fa48632f0c08ac89e8e18a
Tomer Simis <tdl.simis@gmail.com>
2014-11-05 18:41:53 -0300

    Update Pagamento.sql

--- Pagamento.sql
+++ Pagamento.sql
@@ -1,29 +1,29 @@
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (10022334, SYSDATE , 'A vista', 1000.20);
+values (10022334, TO_DATE('12/09/2014', 'dd/mm/yyyy') , 'A vista', 1000.20);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (10034, SYSDATE, 'Boleto', 2953.70);
+values (10034, TO_DATE('12/10/2014', 'dd/mm/yyyy'), 'Boleto', 2953.70);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (1004, SYSDATE, 'Credito', 765.80);
+values (1004, TO_DATE('12/12/2014', 'dd/mm/yyyy'), 'Credito', 765.80);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (10, SYSDATE, 'Credito', 674.00);
+values (10, TO_DATE('15/11/2014', 'dd/mm/yyyy'), 'Credito', 674.00);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (1002, SYSDATE, 'A vista', 235.50);
+values (1002, TO_DATE('18/10/2014', 'dd/mm/yyyy'), 'A vista', 235.50);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (4, SYSDATE, 'Boleto', 321.80);
+values (4, TO_DATE('19/11/2014', 'dd/mm/yyyy'), 'Boleto', 321.80);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (14, SYSDATE, 'Deposito', 500.90);
+values (14, TO_DATE('03/12/2014', 'dd/mm/yyyy'), 'Deposito', 500.90);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (124, SYSDATE, 'Credito', 150.10);
+values (124, TO_DATE('27/12/2014', 'dd/mm/yyyy'), 'Credito', 150.10);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (10334, SYSDATE, 'A vista', 750.20);
+values (10334, TO_DATE('09/10/2014', 'dd/mm/yyyy'), 'A vista', 750.20);
 
 insert into Pagamento (Codigo, Data_de_Pagamento, Tipo_de_pagamento, Valor)
-values (9422334, SYSDATE, 'Deposito', 343.20);
+values (9422334, TO_DATE('01/11/2014', 'dd/mm/yyyy'), 'Deposito', 343.20);
