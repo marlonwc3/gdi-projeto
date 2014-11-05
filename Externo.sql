8cc7d2746a2de4404015f01bbb41d4b504bb593b
Tomer Simis <tdl.simis@gmail.com>
2014-11-05 18:45:45 -0300

    Update Externo.sql

--- Externo.sql
+++ Externo.sql
@@ -1,5 +1,5 @@
-INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-1','Baixo','Total');
-INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-2','Alto','Total');
-INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '000.000.000-3','Alto','ParcialB1');
-INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '000.000.000-4','Baixo','ParcialB2');
-INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-5','Baixo','ParcialB3');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-6','Baixo','Total');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-7','Alto','Total');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '000.000.000-8','Alto','ParcialB1');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('23.13.131/112-12', '000.000.000-9','Baixo','ParcialB2');
+INSERT INTO Externo (Loja_CNPJ,Funcionario_CPF,Grau_de_responsabilidade,Tipo_de_acesso) VALUES('86.814.375/0001-00', '000.000.000-0','Baixo','ParcialB3');
