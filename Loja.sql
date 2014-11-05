98591864cc4295d80c98ad2b3da137abb626a7b3
pazspm <pazspm@users.noreply.github.com>
2014-11-05 18:43:29 -0300

    Update Loja.sql

--- Loja.sql
+++ Loja.sql
@@ -1,39 +1,39 @@
 # Loja
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('86.814.375/0001-00', 'Roupas', 'contato@cea.com.br', 'Jovem', 'Á vista/Á prazo', SYSDATE, 'CeA');
+VALUES ('86.814.375/0001-00', 'Roupas', 'contato@cea.com.br', 'Jovem', 'Á vista/Á prazo', TO_DATE('01/07/2014', 'dd/mm/yyyy'), 'CeA');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('83.824.371/0001-00', 'Restaurante', 'contato@boteco.com.br', 'Adulto', 'Á vista/Á prazo', SYSDATE, 'Boteco - Bar e Restaurante');
+VALUES ('83.824.371/0001-00', 'Restaurante', 'contato@boteco.com.br', 'Adulto', 'Á vista/Á prazo', TO_DATE('12/03/2014', 'dd/mm/yyyy'), 'Boteco - Bar e Restaurante');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('33.824.141/0002-00', 'Farmácia', 'contato@santamaria.com.br', 'Idoso', 'Á vista/Á prazo', SYSDATE, 'Farmácia Santa Maria');
+VALUES ('33.824.141/0002-00', 'Farmácia', 'contato@santamaria.com.br', 'Idoso', 'Á vista/Á prazo',TO_DATE('12/07/2011', 'dd/mm/yyyy'), 'Farmácia Santa Maria');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('13.14.141/132-00', 'Restaurante', 'hablaconosco@donandaluz.com.br', 'Maratonista', 'Dinero', SYSDATE, 'Don Andaluz');
+VALUES ('13.14.141/132-00', 'Restaurante', 'hablaconosco@donandaluz.com.br', 'Maratonista', 'Dinero', TO_DATE('12/09/2000', 'dd/mm/yyyy'), 'Don Andaluz');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('13.11.111/112-00', 'Livraria', 'contato@saraiva.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Livraria Saraiva');
+VALUES ('13.11.111/112-00', 'Livraria', 'contato@saraiva.com.br', 'Todos', 'Á vista/Á prazo', TO_DATE('12/07/2001', 'dd/mm/yyyy'), 'Livraria Saraiva');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('23.13.131/112-12', 'Ótica', 'contato@oticasdiniz.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Óticas Diniz');
+VALUES ('23.13.131/112-12', 'Ótica', 'contato@oticasdiniz.com.br', 'Todos', 'Á vista/Á prazo', TO_DATE('12/01/2011', 'dd/mm/yyyy'), 'Óticas Diniz');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('23.11.121/132-11', 'Cinema', 'contato@uci.com.br', 'Jovem', 'Á vista/Á prazo', SYSDATE, 'Cinema UCI');
+VALUES ('23.11.121/132-11', 'Cinema', 'contato@uci.com.br', 'Jovem', 'Á vista/Á prazo', TO_DATE('01/02/2013', 'dd/mm/yyyy'), 'Cinema UCI');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('93.15.128/112-11', 'Loja', 'contato@eletroshopping.com.br', 'Adulto', 'Á vista/Á prazo', SYSDATE, 'Eletroshopping');
+VALUES ('93.15.128/112-11', 'Loja', 'contato@eletroshopping.com.br', 'Adulto', 'Á vista/Á prazo', TO_DATE('12/03/2012', 'dd/mm/yyyy'), 'Eletroshopping');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('82.43.132/193-63', 'Mercado', 'contato@hiper.com.br', 'Todos', 'Á vista/Á prazo', SYSDATE, 'Hiper-Bombreço');
+VALUES ('82.43.132/193-63', 'Mercado', 'contato@hiper.com.br', 'Todos', 'Á vista/Á prazo', TO_DATE('03/02/2010', 'dd/mm/yyyy'), 'Hiper-Bombreço');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('58.34.321/132-43', 'Loja', 'contato@rommanel.com.br', 'Adulto', 'Á vista', SYSDATE, 'Rommanel Jóias');
+VALUES ('58.34.321/132-43', 'Loja', 'contato@rommanel.com.br', 'Adulto', 'Á vista', TO_DATE('12/02/2014', 'dd/mm/yyyy'), 'Rommanel Jóias');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('58.34.231/345-43', 'Livraria', 'contato@livrosa.com.br', 'Maratonista', 'Á vista', SYSDATE, 'Livros SA');
+VALUES ('58.34.231/345-43', 'Livraria', 'contato@livrosa.com.br', 'Maratonista', 'Á vista', TO_DATE('11/11/2011', 'dd/mm/yyyy'), 'Livros SA');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('49.14.321/132-43', 'Livraria', 'contato@cultura.com.br', 'Maratonista', 'Á vista', SYSDATE, 'Cultura');
+VALUES ('49.14.321/132-43', 'Livraria', 'contato@cultura.com.br', 'Maratonista', 'Á vista', TO_DATE('03/07/2003', 'dd/mm/yyyy'), 'Cultura');
 
 INSERT INTO Loja (CNPJ, Tipo, Email, Publico_alvo, Forma_de_pagamento, Data_de_abertura, Nome_da_loja) 
-VALUES ('29.34.856/938-941', 'Eletronicos', 'contato@infobox.com.br', 'Maratonista', 'Á vista', SYSDATE, 'Infobox');
+VALUES ('29.34.856/938-941', 'Eletronicos', 'contato@infobox.com.br', 'Maratonista', 'Á vista', TO_DATE('12/01/2012', 'dd/mm/yyyy'), 'Infobox');
